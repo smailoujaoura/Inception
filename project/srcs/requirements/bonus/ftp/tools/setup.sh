6c6b5b116524470d48e02a12f3e7ee8748b8acde
Smail Oujaoura <email.oujaoura@gmail.com>
2025-07-16 18:25:29 -0400

    finis

--- project/srcs/requirements/bonus/ftp/tools/setup.sh
+++ project/srcs/requirements/bonus/ftp/tools/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -x
 
 mkdir -p /var/run/vsftpd/empty
@@ -8,12 +7,13 @@ chmod 755 /var/run/vsftpd/empty
 
 useradd -m $FTP_USER
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
 mkdir -p /home/$FTP_USER/ftp
-chown nobody:nogroup /home/$FTP_USER/ftp
+chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp
+chmod 755 /home/$FTP_USER/ftp
 
 mkdir -p /home/$FTP_USER/ftp/wordpress
 mount --bind /var/www/html /home/$FTP_USER/ftp/wordpress
-
 chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/wordpress
 
 exec /usr/sbin/vsftpd /etc/vsftpd.conf
